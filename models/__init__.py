c3b5b1eb5122e518fb660345985f26e5f127102b
Michael <mm.tarekegn@gmail.com>
2026-06-12 11:58:56 -0700

    Add models/ directory and allen_cahn.mat from chlorwu/tmc-pinn baseline

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+﻿from .PINN import Model as PINN
+from .QRes import Model as QRes
+from .FLS import Model as FLS
+from .KAN import Model as KAN
+from .PINNsFormer import Model as PINNsFormer
+from .PINNsFormer_Enc_Only import Model as PINNsFormer_Enc_Only
+from .PINNMamba import Model as PINNMamba
